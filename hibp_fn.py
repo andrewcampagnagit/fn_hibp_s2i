b1f713d2a2193f0c37fcd08d7a22536d43618587
Andrew <59933938+andrewcampagnagit@users.noreply.github.com>
2024-02-29 02:02:07 -0600

    Update hibp_fn.py

--- hibp_fn.py
+++ hibp_fn.py
@@ -1,1 +1,18 @@
+import requests
+import os
 
+url = "https://haveibeenpwned.com/api/v3/breachedaccount/andrewalex@optonline.net"
+payload={}
+api_key = os.environ["APP_INPUTS"]
+
+headers = {
+  "hibp-api-key": api_key,
+  "format": "application/json",
+  "timeout": "2.5",
+  "HIBP": api_key,
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+print("Done!")
